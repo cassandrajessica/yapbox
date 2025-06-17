a6363e906963cbc8a05c24edbebd26baafa87b46
Cassandra Lehman <cassandrajessica@Cassandras-Air.attlocal.net>
2025-06-17 14:54:25 -0500

    added react router dom to view components

--- reactChatApp/src/App.jsx
+++ reactChatApp/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Login from './components/Login';
+import AccountSetup from './components/AccountSetup';
 function App() {
 
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/setup' element={<AccountSetup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
