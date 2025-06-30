b4a31f6c24e337bbc467672386c2260d436d5be4
Cassandra Lehman <cassandrajessica@Cassandras-Air.attlocal.net>
2025-06-30 15:12:38 -0500

    added Chat page to browser routes

--- reactChatApp/src/App.jsx
+++ reactChatApp/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Login from './components/Login';
 import AccountSetup from './components/AccountSetup';
+import ChatPage from './components/ChatPage';
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/setup' element={<AccountSetup />} />
+          <Route path='/chat' element={<ChatPage />}/>
         </Routes>
       </BrowserRouter>
     </>
